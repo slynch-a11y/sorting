bd00d2ead081dcf4448942fd382a6fba1c3382d0
Sarah Lynch <sarah@alum.rpi.edu>
2018-06-11 15:51:06 -0400

    work in progress

--- mergesort.js
+++ mergesort.js
@@ -1,12 +1,40 @@
 function split(wholeArray) {
+  if (wholeArray.length === 1){
+    return wholeArray;
+  }
 
-  /* your code here to define the firstHalf and secondHalf arrays */
+ let midpoint = Math.floor(wholeArray.length/2);
+ let firstHalf = wholeArray.slice(0, midpoint);
+ let secondHalf = wholeArray.slice(midpoint, wholeArray.length);
 
   return [firstHalf, secondHalf];
 }
 
 function mergeSort(array) {
+const finalArr = [];
+let firstCounter = 0;
+let secondCounter = 0;
+let firstHalf = array[0];
+let secondHalf = array[1];
 
-  /* your code here */
+
+while (firstCounter < firstHalf.length && secondCounter < secondHalf.length){
+  if (firstHalf[firstCounter] < secondHalf[secondCounter]){
+    finalArr.push(firstHalf[firstCounter]);
+    firstCounter++;
+  }
+  else {
+    finalArr.push(secondHalf[secondCounter]);
+    secondCounter++;
+  }
 
 }
+// let finalAns = finalArr.concat(firstHalf.slice(firstCounter)).concat(secondHalf.slice(secondCounter));
+
+mergeSort(split(firstHalf));
+mergeSort(split(secondHalf));
+
+
+}
+
+console.log(mergeSort([3, 2, 1, 4, 5]));
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,11 +1,13 @@
 describe('Split Array function', function() {
   it('is able to split an array into two halves', function() {
-    // your code here
+    var result = split([3, 2, 1, 4, 5]);
+		expect(result).toEqual([[3,2], [1, 4, 5]]);
   });
 });
 
 describe('Merge function', function(){
   it('is able to merge two sorted arrays into one sorted array', function(){
-    // test the merging algorithm
+    var result = mergeSort([3, 5, 1, 89, 20, 2]);
+		expect(result).toEqual([1, 2, 3, 5, 20, 89]);
   });
 });
